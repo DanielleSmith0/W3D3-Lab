83e9b9d9f0d752ec0302c07afc39bd5e34fd0879
DanielleSmith0 <daniellesmith@Danielles-MBP.localdomain>
2023-03-01 16:33:53 -0700

    core complete

---
+++ index.js
@@ -0,0 +1,38 @@
+let inputField = document.querySelector("input");
+let form = document.querySelector("form");
+let message = document.querySelector("#message");
+
+function addMovie(event) {
+    event.preventDefault();
+    let movie = document.createElement("li");
+    let movieTitle = document.createElement("span");
+    let list = document.querySelector("ul");
+
+    movieTitle.textContent = inputField.value
+    movieTitle.addEventListener("click", crossOffMovie);
+    movie.appendChild(movieTitle);
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X"
+    deleteBtn.addEventListener("click", deleteMovie);
+    movie.appendChild(deleteBtn);
+
+    list.appendChild(movie);
+    inputField.value = ""
+}
+
+form.addEventListener("submit", addMovie);
+
+function deleteMovie(event) {
+    event.target.parentNode.remove()
+    message.textContent = "Movie deleted!"
+}
+
+function crossOffMovie(event) {
+    event.target.classList.toggle("checked");
+    if (event.target.classList.contains("checked")) {
+        message.textContent = "Movie watched!"
+    }else {
+        message.textContent = "Movie added back!"
+    }
+}
